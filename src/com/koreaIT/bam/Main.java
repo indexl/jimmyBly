ff236eb8e47660a6ac4ef6f4f4681dc580885347
indexl <indexl@naver.com>
2024-10-08 15:22:39 +0900

    게시판틀잡기

---
+++ src/com/koreaIT/bam/Main.java
@@ -0,0 +1,47 @@
+package com.koreaIT.bam;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine(); //보통 이걸로			
+			
+			
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
+		
+		sc.close();
+		
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
